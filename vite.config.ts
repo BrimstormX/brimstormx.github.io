ef1b6a940db855cbcecdbf5785f524ef1a3409a5
BrimstormX <Youssef@VAHorizon.site>
2025-08-23 14:35:30 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
+    resolve: {
+    alias: {
+      'Lucide-react': 'lucide-react',
+      'sonner@2.0.3': 'sonner'
+    }
+  },
+
   build: { outDir: 'dist' }
 })
+  
